ad0ead091c5dea28248257971fb5727ed4649a62
Renan Franca <renanfranca.dev@gmail.com>
2025-11-03 20:14:29 -0300

    feat: create User and Task models

---
+++ Models/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+        // FK to User
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; } 
+        public DateTime CreatedAt { get; set; }
+
+        // Relacionamento: 1 User tem N Tasks
+        public List<Task> Tasks { get; set; }
+    }
+}
